1323419477b4a1740c358a70162f8689070c960e
Angel David Trinidad Delgado <37111032+AngelTrinidad@users.noreply.github.com>
2019-10-05 04:52:28 -0400

    nuevo proyecto

--- pacientesveterinarias/src/components/ListaCitas.js
+++ pacientesveterinarias/src/components/ListaCitas.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Cita from './Cita';
 
 const ListaCitas = ({citas, eliminarCita}) => {
+
+    //Imprimir el mensaje
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aqui';
+
     return (
         <div className="card mt-2 py-5">
             <div className="card-body">
-                <h2 className="card-title text-center">Administra las citas aqui</h2>
+                <h2 className="card-title text-center">{mensaje}</h2>
                 <div className="lista-citas">
                     {citas.map(e => (
                         <Cita 
