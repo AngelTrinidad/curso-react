5a12ae75fe2639d66e1fd7d328e2c6935dfde281
Angel Trinidad <96trinidad@gmail.com>
2019-10-14 09:07:04 -0300

    cambios

--- routercrudreact/src/App.js
+++ routercrudreact/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import axios from 'axios';
+
+
 import AgregarProductos from './components/AgregarProductos';
 import Productos from './components/Productos';
 import EditarProductos from './components/EditarProductos';
 import Producto from './components/Producto';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 
 function App() {
+
+  const urlAPI = 'http://localhost:4000';
+
+  const [productos, guardarProductos] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const res = await axios.get(`${urlAPI}/restaurant`);
+      
+      guardarProductos(res.data);
+    }
+    consultarAPI();
+  }, []);
+
   return (
     <Router>
       <Header /> {/*Como un include a todas las paginas dentro del router*/}
@@ -18,8 +35,8 @@ function App() {
           <Route exact path="/productos/editar/:id" component={EditarProductos}/>
           <Route exact path="/productos/:id" component={Producto}/>
         </Switch>
-        <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
       </main>
+      <Footer/>
     </Router>
   );
 }
---
+++ routercrudreact/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
